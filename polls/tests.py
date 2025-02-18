3b8da1d30518dfadec3f7b98ca510372072e6c42
Ale Guillén <alejandraeg9899@gmail.com>
2025-02-18 12:56:36 -0600

    Test Case

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,68 @@
+from django.test import Client
+from django.urls import reverse
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
 
 import datetime
+
+def create_question(question_text, days):
+    '''
+    Publicación de una pregunta junto con el día
+    '''
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        '''
+        Si la pregunta no existe se lanza un msj
+        '''
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No hay encuestas disponibles.')
+        self.assertQuerySetEqual(response.content['latest_question_list'], [])
+
+    def test_past_question(self):
+        '''
+        Las preguntas con fecha de publicación se muestran en el índice.
+        '''
+        question = create_question(question_text='Encuestas pasadas.', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerySetEqual(
+            response.content['latest_question_list'], [question],
+        )
+
+    def test_future_question(self):
+        '''
+        Las preguntas con fecha de publicación futura no se muestran en la pág del índice.
+        '''
+        create_question(question_text='Preguntas futuras.', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, 'No hay encuestas disponibles.')
+        self.assertQuerySetEqual(response.content['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        '''
+        Solo se muestran las preguntas con fecha de publicación vigente
+        '''
+        question = create_question(question_text='Preguntas pasadas.', days=-30)
+        create_question(question_text='Preguntas futuras.', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerySetEqual(
+            response.content['latest_question_list'], [question],
+        )
+
+    def test_two_past_questions(self):
+        '''
+        La pág índice puede mostrar varias preguntas.
+        '''
+        question1 = create_question(question_text='Pregunta pasada 1.', days=-30)
+        question2 = create_question(question_text='Pregunta pasada 2.', days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerySetEqual(
+            response.content['latest_question_list'], [question2, question1],
+        )
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
@@ -28,3 +88,22 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        '''
+        La vista de una pregunta en el futuro devuelve un error 404.
+        '''
+        future_question = create_question(question_text='Preguntas futuras.', days=5)
+        url = reverse('polls:detail', args=(future_question.id, ))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        '''
+        La vista de una pregunta en el pasado muestra el texto de la pregnta.
+        '''
+        past_question = create_question(question_text='Preguntas pasadas.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id, ))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.utils import timezone
 from django.views import generic
 from django.http import Http404
 from django.urls import reverse
@@ -16,12 +17,18 @@ class IndexView(generic.ListView):
         '''
         Devolver las ultimas 5 cinco preguntas publicadas
         '''
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        '''
+        Se excluyen las preguntas que aún no han sido publicadas.
+        '''
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
